5ac3d70440f2c4e9e62546ff4add2604bbf6ab80
wiktor <nobis171@wp.pl>
2024-06-15 10:40:34 +0200

    mutations query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import './App.css'
-
-import {useQuery, useMutation } from "react-query";
+import './App.css';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 
 interface Post {
-    id: number;
+    id: number | string;
     title: string;
 }
 
@@ -12,26 +11,48 @@ const POSTS: Post[] = [
     { id: 2, title: "Post 2" },
 ];
 
-function App() {
+const wait = (duration: number): Promise<void> => {
+    return new Promise(resolve => setTimeout(resolve, duration));
+};
+
+const addPost = async (title: string): Promise<void> => {
+    await wait(1000);
+    POSTS.push({ id: crypto.randomUUID(), title });
+};
+
+
+const App = () => {
+    const queryClient = useQueryClient();
     const postsQuery = useQuery<Post[]>({
         queryKey: ["posts"],
         queryFn: () => wait(1000).then(() => [...POSTS]),
-    })
+    });
 
-    if(postsQuery.isLoading) return <h1>Loading...</h1>
-    if(postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>
+    const newPostMutations = useMutation<void, Error, string>({
+        mutationFn: addPost,
+        onSuccess: () => {
+            queryClient.invalidateQueries(["posts"]);
+        },
+        onError: (error) => {
+            console.error("Error adding post:", error);
+        }
+    });
+
+    if (postsQuery.isLoading) return <h1>Loading...</h1>;
+    if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
 
     return (
         <div>
             {postsQuery.data?.map(post => (
                 <div key={post.id}>{post.title}</div>
             ))}
+            <button disabled={newPostMutations.isLoading} onClick={() => newPostMutations.mutate("New Post")}>
+                Add new
+            </button>
         </div>
     );
-}
-
-function wait(duration: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, duration))
 };
 
-export default App
+
+
+export default App;
