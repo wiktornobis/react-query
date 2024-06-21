0c586364008b07ace0ced7db0a0e293555b22f12
wiktor <nobis171@wp.pl>
2024-06-21 11:04:48 +0200

    overwrite default stale time in particular fetch

--- src/api/areas/queries.ts
+++ src/api/areas/queries.ts
@@ -11,6 +11,7 @@ export const useAREAS = () =>
     useQuery<BasicResponse<Areas[]>, Error>({
         queryKey: [QueryKeys.AREAS],
         queryFn: fetchAreas,
+        staleTime: 1000 * 10, //10 sekund - ten kod nadpisuje stan w main.tsx (default 5 minut)
     });
 
 export const prefetchAREAS = async (queryClient: QueryClient) =>
